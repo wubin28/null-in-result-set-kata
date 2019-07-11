1ccf18aba0ec40c6047c83bb4dffa546387a5a0c
benwu <wubinben@icloud.com>
2019-07-11 22:32:58 +0800

    query_查询订单收货人

--- src/main/java/kata/resultset/Order.java
+++ src/main/java/kata/resultset/Order.java
@@ -1,7 +1,41 @@
 package kata.resultset;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Order {
-    public double querySum(String orderId) {
-        return 27.4;
+
+    public String queryReceiverName(String orderId) {
+        String receiverName = "";
+        Connection connection = null;
+        try {
+
+            String url = "jdbc:sqlserver://localhost:42588;";
+
+            connection = DriverManager.getConnection(url, "admin", "root");
+
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("select receiver_name from orders where order_id = ?");
+            preparedStatement.setString(1, orderId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next() == false) {
+                receiverName = "";
+            } else {
+                do {
+                    receiverName = resultSet.getString(1);
+                    if (receiverName == null) {
+                        receiverName = "";
+                    }
+                } while (resultSet.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return receiverName;
     }
 }
--- src/test/java/kata/resultset/OrderTest.java
+++ src/test/java/kata/resultset/OrderTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 
 public class OrderTest {
     @Test
-    public void query_查询订单金额()
+    public void query_查询订单收货人()
     {
         Order order = new Order();
-        assertEquals(27.4, order.querySum("26655056069649850"), 0.005);
+        assertEquals("张先生", order.queryReceiverName("26655056069649850"));
     }
 }
